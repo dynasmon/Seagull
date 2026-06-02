71480b2bd66ced3620c7d4e5b7f80f883ac66b55
nathanmblima <nathanmblima@gmail.com>
2026-06-02 14:28:32 -0300

    added eui filters and charts

---
+++ frontend/src/shared/components/charts/index.ts
@@ -0,0 +1,20 @@
+export { ChartFrame } from "./ChartFrame";
+export type { ChartFrameProps } from "./ChartFrame";
+
+export { TimeSeriesChart } from "./TimeSeriesChart";
+export type { TimeSeriesChartProps } from "./TimeSeriesChart";
+
+export { BarChart } from "./BarChart";
+export type { BarChartProps } from "./BarChart";
+
+export { DonutChart } from "./DonutChart";
+export type { DonutChartProps, DonutDatum } from "./DonutChart";
+
+export {
+  useEuiChartTheme,
+  useSeverityChartColors,
+  useSeriesColorResolver,
+} from "./chartTheme";
+export type { EuiChartTheme } from "./chartTheme";
+
+export { maskTrailingNoDataBuckets } from "./maskTrailingNoData";
