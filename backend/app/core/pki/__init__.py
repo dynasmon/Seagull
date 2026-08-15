c85aa847319ce1859e0f973d5fbf6ccf1f264b63
Nathan <nathanmblima@gmail.com>
2026-08-15 13:47:12 -0300

    feat: export the certificate authority client

---
+++ backend/app/core/pki/__init__.py
@@ -0,0 +1,13 @@
+from app.core.pki.signer import (
+    SignedCertificate,
+    SignerRejected,
+    SignerUnavailable,
+    sign_agent_certificate,
+)
+
+__all__ = [
+    "SignedCertificate",
+    "SignerRejected",
+    "SignerUnavailable",
+    "sign_agent_certificate",
+]
